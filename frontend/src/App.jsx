60669444b3b219245218c60dd7cfa6ae295e6ba3
Mike Makori <157876022+bluntpencil254@users.noreply.github.com>
2026-06-30 13:33:44 +0300

    Update weather API URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     }
 
     const res = await fetch(
-      `http://127.0.0.1:5000/weather/${city}`
+      `https://myweatherapp-a5k5.onrender.com/weather/${city}`
     );
 
     const data = await res.json();
